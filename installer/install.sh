08d92dc80a58f4cc0194493597087df169abf3c0
Simon Mertins <mertins99@gmail.com>
2024-10-17 20:22:16 +0200

    installscriptw

---
+++ installer/install.sh
@@ -0,0 +1,3 @@
+for installer in ~/dotfiles/installer/apps/*.sh; do source $installer; done
+for installer in ~/dotfiles/installer/apps/*.sh; do source $installer; done
+for installer in ~/dotfiles/installer/post/*.sh; do source $installer; done
