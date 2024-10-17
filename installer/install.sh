686b4673ee412b1609596731f2d1626a0cae0da0
Simon Mertins <mertins99@gmail.com>
2024-10-17 20:29:10 +0200

    new update

--- installer/install.sh
+++ installer/install.sh
@@ -1,3 +1,7 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y curl git unzip
+
 for installer in ~/dotfiles/installer/apps/*.sh; do source $installer; done
 for installer in ~/dotfiles/installer/apps/*.sh; do source $installer; done
 for installer in ~/dotfiles/installer/post/*.sh; do source $installer; done
