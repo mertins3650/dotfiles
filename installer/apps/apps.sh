8db6b6950bce93663c9802336e7df8da5e2b8b5b
Simon Mertins <mertins99@gmail.com>
2024-10-17 20:23:43 +0200

    new

--- installer/apps/apps.sh
+++ installer/apps/apps.sh
@@ -68,3 +68,20 @@ sudo install zellij /usr/local/bin
 rm zellij.tar.gz zellij
 ln -s ~/dotfiles/zellij/ ~/.config/zellij/
 cd -
+
+#fontmkdir
+mkdir -p ~/.local/share/fonts
+
+cd /tmp
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
+unzip CascadiaMono.zip -d CascadiaFont
+cp CascadiaFont/*.ttf ~/.local/share/fonts
+rm -rf CascadiaMono.zip CascadiaFont
+
+wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
+unzip iafonts.zip -d iaFonts
+cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
+rm -rf iafonts.zip iaFonts
+
+fc-cache
+cd -
