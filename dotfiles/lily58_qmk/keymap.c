c9a5f6abeb9c9ae939cc5f1c818e185d61e69528
Simon Mertins <mertins99@gmail.com>
2024-12-08 14:15:08 +0100

    clean up layout

--- dotfiles/lily58_qmk/keymap.c
+++ dotfiles/lily58_qmk/keymap.c
@@ -1,7 +1,6 @@
 #include QMK_KEYBOARD_H
 
 
-
 enum layer_number {
   _QWERTY = 0,
   _LOWER,
@@ -15,27 +14,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
  [_QWERTY] = LAYOUT(
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                             KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
-  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-  KC_EXT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LSFT(KC_1), LSFT(KC_MINS),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
-                   KC_LALT, KC_LSFT,  KC_LGUI, LCTL_T(KC_SPC), RALT_T(KC_ENT), KC_BSPC, KC_SYM, KC_PSCR
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                              KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+  KC_EXT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                              KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, LSFT(KC_1),  LSFT(KC_MINS),  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
+                   KC_LALT, KC_LSFT,  KC_LGUI, LCTL_T(KC_SPC),    RALT_T(KC_ENT), KC_BSPC, KC_SYM, KC_PSCR
 ),
 
  [_LOWER] = LAYOUT(
-  _______,KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  
-  _______, KC_BRID, KC_BRIU, _______, _______, _______,                   KC_PGDN, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
-  KC_LSFT, KC_VOLD, KC_VOLU, _______, _______, _______,                   _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-  KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-                             _______, _______, _______, _______, _______,  _______, _______, _______
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  
+  _______, KC_BRID, KC_BRIU, _______, _______, _______,                    KC_PGDN, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
+  KC_LSFT, KC_VOLD, KC_VOLU, _______, _______, _______,                    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+  KC_LCTL, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, _______,
+                             _______, _______, _______, _______,  _______, _______, _______, _______
 ),
 
 [_RAISE] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                                            _______, _______, _______, _______, _______, _______,
-  KC_GRV,  RALT(KC_2),    LSFT(KC_EQL),    KC_MINS,    LSFT(KC_7), RALT(KC_RBRC),                  LSFT(KC_5), RALT(KC_NUBS),  LSFT(KC_0), KC_EQL, LSFT(KC_3),  _______,
-  LSFT(KC_NUHS),   LSFT(KC_SLSH),  RALT(KC_7), RALT(KC_8), LSFT(KC_8), LSFT(KC_2),                 KC_BSLS, LSFT(KC_9), RALT(KC_9), RALT(KC_0), RALT(KC_4), XXXXXXX,
-  KC_LCTL,  RALT(KC_E), RALT(KC_3),  LSFT(KC_6), KC_NUBS,  _______, LSFT(KC_RBRC), KC_RBRC,  LSFT(KC_GRV), LSFT(KC_NUBS), RALT(KC_EQL),  KC_LBRC, KC_RBRC, KC_BSLS,
-                             _______, _______, _______,  _______, _______,  _______, _______, _______
+  _______,       _______,       _______,      _______,    _______,    _______,                               _______,      _______,       _______,      _______,    _______,    _______,
+  KC_GRV,        RALT(KC_2),    LSFT(KC_EQL), KC_MINS,    LSFT(KC_7), RALT(KC_RBRC),                         LSFT(KC_5),   RALT(KC_NUBS), LSFT(KC_0),   KC_EQL,     LSFT(KC_3), _______,
+  LSFT(KC_NUHS), LSFT(KC_SLSH), RALT(KC_7),   RALT(KC_8), LSFT(KC_8), LSFT(KC_2),                            KC_BSLS,      LSFT(KC_9),    RALT(KC_9),   RALT(KC_0), RALT(KC_4), XXXXXXX,
+  KC_LCTL,       RALT(KC_E),    RALT(KC_3),   LSFT(KC_6), KC_NUBS,    _______,       LSFT(KC_RBRC), KC_RBRC, LSFT(KC_GRV), LSFT(KC_NUBS), RALT(KC_EQL), KC_LBRC,    KC_RBRC,    KC_BSLS,
+                                              _______,    _______,    _______,       _______,       _______, _______,      _______,       _______
 ),
 };
 
