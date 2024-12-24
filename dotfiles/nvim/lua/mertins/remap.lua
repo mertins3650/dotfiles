0b11c3cdcc3453951d663697f17045977ce44d4a
Simon Mertins <mertins99@gmail.com>
2024-12-24 08:06:03 +0100

    keymaps

--- dotfiles/nvim/lua/mertins/remap.lua
+++ dotfiles/nvim/lua/mertins/remap.lua
@@ -20,3 +20,5 @@ vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
