900ade9f571bb631958106a2ee81952b70e6c830
Simon Mertins <simon@mertins.net>
2025-12-25 19:20:20 +0100

    supermaven

--- nvim/.config/nvim/lua/mertins/lazy/blink.lua
+++ nvim/.config/nvim/lua/mertins/lazy/blink.lua
@@ -1,7 +1,7 @@
 return {
 	"saghen/blink.cmp",
 	version = "*",
-	dependencies = { "L3MON4D3/LuaSnip", version = "v2.*", "milanglacier/minuet-ai.nvim" },
+	dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
 	opts = {
 		keymap = { ["<C-k>"] = nil },
 		snippets = { preset = "luasnip" },
@@ -10,18 +10,7 @@ return {
 			nerd_font_variant = "mono",
 		},
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "minuet" },
-			providers = {
-				minuet = {
-					name = "minuet",
-					module = "minuet.blink",
-					async = true,
-					-- Should match minuet.config.request_timeout * 1000,
-					-- since minuet.config.request_timeout is in seconds
-					timeout_ms = 3000,
-					-- score_offset = 50, -- Gives minuet higher priority among suggestions
-				},
-			},
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 		completion = {
 			trigger = { prefetch_on_insert = false },
