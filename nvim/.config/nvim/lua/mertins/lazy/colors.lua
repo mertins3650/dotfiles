d1cd9ef7584890be0e479e1ccd0a0201b6ffc69b
Simon Mertins <simon@mertins.net>
2026-01-13 10:06:44 +0100

    catp

--- nvim/.config/nvim/lua/mertins/lazy/colors.lua
+++ nvim/.config/nvim/lua/mertins/lazy/colors.lua
@@ -1,51 +1,9 @@
 function ColorMyPencils()
 	-- vim.cmd.colorscheme("tokyonight")
-	vim.cmd.colorscheme("rose-pine-moon")
-	-- transparent background
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "Terminal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "Folded", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "none" })
-	--
-	-- -- transparent background for neotree
-	-- vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NeoTreeVertSplit", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NeoTreeWinSeparator", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NeoTreeEndOfBuffer", { bg = "none" })
-	--
-	-- -- transparent background for nvim-tree
-	-- vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NvimTreeVertSplit", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { bg = "none" })
-	--
-	-- -- transparent notify background
-	-- vim.api.nvim_set_hl(0, "NotifyINFOBody", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyERRORBody", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyWARNBody", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyTRACEBody", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyDEBUGBody", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyINFOTitle", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyERRORTitle", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyWARNTitle", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyTRACETitle", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyDEBUGTitle", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyINFOBorder", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyERRORBorder", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyWARNBorder", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NotifyDEBUGBorder", { bg = "none" })
+	-- vim.cmd.colorscheme("rose-pine-moon")
+	vim.cmd.colorscheme("catppuccin-macchiato")
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 -- return {
@@ -58,17 +16,27 @@ end
 -- 		end,
 -- 	},
 -- }
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			disable_background = true,
+-- 			styles = {
+-- 				italic = false,
+-- 			},
+-- 		})
+--
+-- 		ColorMyPencils()
+-- 	end,
+-- }
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	config = function()
-		require("rose-pine").setup({
-			disable_background = true,
-			styles = {
-				italic = false,
-			},
+		require("catppuccin").setup({
+			styles = {},
 		})
-
 		ColorMyPencils()
 	end,
 }
